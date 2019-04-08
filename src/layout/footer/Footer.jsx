70a93fcea1a51e4954e78658f710347dd22abcca
Sri Krishna Teja.k <skamma@epsvjw.local>
2019-04-08 12:46:24 +0530

    curd screen added

---
+++ src/layout/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+        <div className="footer">
+            <p>Footer</p>
+        </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/layout/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+        <div className="header">
+            <p>Header123</p>
+        </div>
+    );
+  }
+}
+
+export default Header;
